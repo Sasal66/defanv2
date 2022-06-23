b2c3d7c7d337895c191fee3ef9abb43a00971e1f
Sasal66 <107730821+Sasal66@users.noreply.github.com>
2022-06-23 19:27:33 +0700

    Delete main-ok.js

--- plugins/main-ok.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/bd6914e065dbd698e72d7.jpg', m, { packname: "sticker by", author: "ArullOfc" })
-}
-
-handler.customPrefix = /^(ok)$/i
-handler.command = new RegExp
-
-module.exports = handler
