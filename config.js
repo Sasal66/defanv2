0585f62506e3d79799ff224bdaeb7b37bf2db399
Sasal66 <107730821+Sasal66@users.noreply.github.com>
2022-06-23 09:16:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 //=============『 Utama 』================== //
-global.owner = ['6285822347348'] 
+global.owner = ['6282135905896'] 
 global.mods = [] 
 global.prems = [''] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ArullOfc'
-global.numberowner = '6285822347348'
-global.instagram = 'https://instagram.com/sahrulwara_____'
-global.github = 'https://github.com/sahrull11'
-global.dana = '085822347348'
-global.pulsa = '085822347348,083843192208'
+global.nameowner = 'ᴹг defα͢͢͢ή彡'
+global.numberowner = '6282135905896'
+global.instagram = 'https://www.instagram.com/invites/contact/?i=1rkt4lh5ewwoa&utm_content=kjc21le'
+global.github = 'https://github.com/Sasal66'
+global.dana = '081321776496'
+global.pulsa = '081321776496'
 global.gopay = ''
 
 //=============『 Info Bot 』=================//
 global.namebot = 'ArullBotzMD'
-global.gc = 'https://chat.whatsapp.com/Hs5rHr27xu49VRLgdliMfR'
-global.web = 'https://chat.whatsapp.com/F6CAAOhrORm1sPdq6cRKAS' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.gc = 'https://chat.whatsapp.com/JVFN0SYnXLg23qjvickkdM'
+global.web = 'https://github.com/Sasal66/Alphabot-MD.git' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = 'Permanen 15.000\n1 bulan 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://telegra.ph/file/d6b8427c43c0bf596c1e2.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/d6b8427c43c0bf596c1e2.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '❚█══ArullBotz‖MD‖'
+global.wm = '❚█══Alphabot‖MD‖'
 global.watermark = wm
-global.wm2 = '                     「 ArullBotzMD あ⁩ 」'
-global.wm3 = '⫹⫺ ❚█══ArullBotz‖MD‖'
+global.wm2 = '                     「 AlphabotMD あ⁩ 」'
+global.wm3 = '⫹⫺ ❚█══Alphabot‖MD‖'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/d6b8427c43c0bf596c1e2.jpg'
@@ -36,8 +36,8 @@ global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*Stiker sedang dibuat*'
-global.packname = 'Sticker By'
-global.author = '© ArullOfc'
+global.packname = 'Sticker By ᴹг defα͢͢͢ή彡'
+global.author = '© Alphabot'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
